4b3803a350100bf79ed0df76c1b48b5d56f123d0
Karan V <karanvknarayanan@gmail.com>
2020-07-12 02:12:04 +0530

    added filters and findin assigned items

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -42,7 +42,8 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short(self):
         # Test password too short
-        payload = {'email': 'test@email.com', 'password': 'Te'}
+        payload = {'email': 'test@email.com',
+                   'password': 'Te', 'name': 'Test', }
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
